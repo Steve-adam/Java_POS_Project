5235f769e5fa8ed4c10dbf4737b155213956b164
adamadam <adamadam4778@gmail.com>
2021-11-23 17:20:46 +0300

    inserted execute update

--- src/com/company/MyJDB.java
+++ src/com/company/MyJDB.java
@@ -29,29 +29,30 @@ public class MyJDB {
     }
 
     void connect(){
-        String id = null;
+        String id = "EDP445361";
         try {
+
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdb", "root", "Deadpool-79sql");
             Statement statement = connection.createStatement();
-            statement.executeUpdate("insert into sales" + "(customer_name,reciept_no,no_of_products,bill,amount_payed)" + "values("+ name +","+ numProducts +","+id+","+bill+","+amount+")");
+            statement.executeUpdate("insert into sales (customer_name, reciept_no, no_of_products, bill, amount_payed) values('"+name+"','"+id+"','"+numProducts+"','"+bill+"','"+amount+"')");
 
-            Order myOrder = new Order(item,quantity,price);
-            myOrders.add(myOrder);
 
-//
+//            Order myOrder = new Order(item,quantity,price);
+//            myOrders.add(myOrder);
 //            statement.executeUpdate("insert into items"+"()");
-//            // Return Values from the Database
-//
-//            ResultSet resultSet = statement.executeQuery("select * from sales");
-//
-////             while resultSet is not empty, continue to print values in DB
-//
-//            while (resultSet.next()) {
-//                System.out.println(resultSet.getString("customer_name"));
-//                System.out.println(resultSet.getString("reciept_no"));
-//                System.out.println(resultSet.getString("no_of_products"));
-//                System.out.println(resultSet.getString("amount_payed"));
-//            }
+            // Return Values from the Database
+
+
+            ResultSet resultSet = statement.executeQuery("select * from sales");
+
+//             while resultSet is not empty, continue to print values in DB
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("customer_name"));
+                System.out.println(resultSet.getString("reciept_no"));
+                System.out.println(resultSet.getString("no_of_products"));
+                System.out.println(resultSet.getString("amount_payed"));
+
+            }
         } catch (SQLException e){
             e.printStackTrace();
         }
